07e8ac47e0bdae43d7f331defb00782bd6f6a101
BrateloSlava <d_slava@list.ru>
2016-02-23 01:11:12 +0100

    Revert "temp disable clang O3"
    This reverts commit fd28e2b6f248bc66d859bae49f09eb6f40ce70e2.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/yuga/bluetooth
 TARGET_KERNEL_CONFIG := fusion3_yuga_defconfig
 
 # Optimizations
-CLANG_O3 := false
+CLANG_O3 := true
 STRICT_ALIASING := false
 KRAIT_TUNINGS := true
 GRAPHITE_OPTS := false
